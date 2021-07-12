8b2a7ec4367cc55dd6e00a315b84002be3a0e55c
M Arslan Riaz <arslanr@emgage.com>
2021-07-12 22:12:55 +0500

    first commit

---
+++ DatingAppSPA/DatingAppSPA/Controllers/ValuesController.cs
@@ -0,0 +1,40 @@
+﻿using DatingAppSPA.Data;
+using DatingAppSPA.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingAppSPA.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ValuesController : ControllerBase
+    {
+       
+
+        private readonly DataContext _dataContext;
+
+        public ValuesController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var values = await _dataContext.values.ToListAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValues(int id)
+        {
+            var values = await _dataContext.values.FirstOrDefaultAsync(x => x.Id == id);
+            return Ok(values);
+        }
+    }
+}
---
+++ DatingAppSPA/DatingAppSPA/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using DatingAppSPA.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingAppSPA.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options)
+        {
+
+        }
+        public DbSet<Value> values { get; set; }
+       
+    }
+}
